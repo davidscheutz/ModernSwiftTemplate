31418f29fdf895307e6eb993156c6c762df9f66d
David Scheutz <scheutz.david+github@gmail.com>
2024-05-04 13:43:31 -0500

    add some onboarding example tests

--- Tests/OnboardingTests/Mocks/AuthenticationManagerMock.swift
+++ Tests/OnboardingTests/Mocks/AuthenticationManagerMock.swift
@@ -1,6 +1,8 @@
+import Foundation
 import Core
 
 final class AuthenticationManagerMock: AuthenticationManager {
+    var delay: TimeInterval = 0
     var shouldLoginPass = true
     var shouldLogoutPass = true
     
@@ -8,11 +10,18 @@ final class AuthenticationManagerMock: AuthenticationManager {
     private(set) var loginCallCount = 0
     private(set) var logoutCallCount = 0
     
-    func login(username: String, password: String) async {
+    func login(username: String, password: String) async throws {
         loginCallCount += 1
+        
+        if delay > 0 {
+            // TODO: SwiftEvoluation Task.sleep(seconds: TimeInterval)
+            try await Task.sleep(nanoseconds: UInt64(delay * 100_000_000))
+        }
         
         if shouldLoginPass {
             isLoggedIn = true
+        } else {
+            throw NSError(domain: "Login failed", code: 400)
         }
     }
     
--- Tests/OnboardingTests/OnboardingTests.swift
+++ Tests/OnboardingTests/OnboardingTests.swift
@@ -1,43 +1,67 @@
 import XCTest
+import SwiftUDF
 @testable import Onboarding
 
 final class OnboardingTests: XCTestCase {
 
+    var sut: LoginLoop!
+    var auth: AuthenticationManagerMock!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        auth = AuthenticationManagerMock()
+        sut = LoginLoop(authenticationManager: auth)
+        sut.start()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        let authMock = AuthenticationManagerMock()
-        let sut = LoginLoop(authenticationManager: authMock)
+    func test_loginWithEmptyInputsResultsInInputErrors() {
+        sut.handle(.login)
         
-//        XCTAssertEqual(sut.currentState, .initial)
-//        
-//        var results = [LoginState]()
-//        var cancellable = sut.state.sink { results.append($0) }
-//        
-//        sut.inputChanged(field: <#T##Input.Field#>, value: <#T##String#>)
-//        
-//        cancellable.cancel()
+        XCTAssertTrue(sut.inputs.hasErrors)
+    }
+    
+    func test_inputIgnoreWhileLoading() {
+        let username = "test"
+        update(username: username)
+        
+        let password = "secret"
+        update(password: password)
+        
+        auth.delay = 0.1
+        sut.handle(.login)
+        
+        update(username: "change attempt")
+        XCTAssertEqual(sut.inputs.value(for: .username), username)
+        
+        update(password: "change attempt")
+        XCTAssertEqual(sut.inputs.value(for: .password), password)
+    }
+    
+    func test_loginFailed() async throws {
+        let username = "test"
+        update(username: username)
+        XCTAssertEqual(sut.inputs.value(for: .username), username)
+        
+        let password = "secret"
+        update(password: password)
+        XCTAssertEqual(sut.inputs.value(for: .password), password)
+        
+        auth.shouldLoginPass = false
+        sut.handle(.login)
+        
+        XCTAssertTrue(sut.isLoading)
+        
+        try await sut.wait { !$0.isLoading }
+        
+        XCTAssertNotNil(sut.error)
+    }
+    
+    // MARK: - Helper
+    
+    private func update(username: String) {
+        sut.handle(.inputChanged(field: .username, value: username))
+    }
+    
+    private func update(password: String) {
+        sut.handle(.inputChanged(field: .password, value: password))
     }
 }
-
-import Combine
-
-// TODO: try to fix 'Escaping closure captures 'inout' parameter 'source''
-extension Publisher where Failure == Never {
-//    func assign(to source: inout [Output]) -> Cancellable {
-//        sink { source.append($0) }
-//    }
-}
-
-// Error: 'Escaping closure captures mutating 'self' parameter'
-//extension Array {
-//    mutating func append<P: Publisher>(from source: P) -> Cancellable where Element == P.Output, P.Failure == Never {
-//        source.sink { append($0) }
-//    }
-//}
