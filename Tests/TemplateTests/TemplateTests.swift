01688c6305d2631c23ded0b270745855e4bd0b05
David Scheutz <scheutz.david+github@gmail.com>
2024-05-05 10:31:50 -0500

    cleanup template tests

--- Tests/TemplateTests/TemplateTests.swift
+++ Tests/TemplateTests/TemplateTests.swift
@@ -1,33 +1,13 @@
 import XCTest
 @testable import Template
-@testable import Onboarding
-import Core
 
 final class TemplateTests: XCTestCase {
 
-    private lazy var authMock = AuthenticationManagerMock()
-    
-    override func setUpWithError() throws {
-        try Dependencies.container.override(AuthenticationManager.self) { self.authMock }
-    }
-
-    func testExample() throws {
-        let sut = LoginLoop.create(with: Dependencies.container)
-        
-//        sut.handle(.inputChanged(field: .username, value: <#T##String#>))
-        
-        sut.handle(.login)
-        
-        // validate username empty
-        
-//        sut.handle(.inputChanged(field: .password, value: <#T##String#>))
-        
-//        sut.handle(.login)
-        
-        // synchrous
-//        sut.currentState
-        
-        // asynchronous
-//        sut.state
+    /// The Template app target is the composition root,
+    /// it's main responsibility is to correctly assemble
+    /// all components. This test ensure all dependencies
+    /// can be resolved.
+    func testDependencies() throws {
+        try Dependencies.verifyResolvability()
     }
 }
