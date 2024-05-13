26d9ff5280ccab488610d931f39dc7e058016c1d
David Scheutz <scheutz.david+github@gmail.com>
2024-05-12 21:30:27 -0500

    add Keychain dependency as SecureStorage implementation

--- Core/Data/Keychain+SecureStorage.swift
+++ Core/Data/Keychain+SecureStorage.swift
@@ -1,3 +1,32 @@
 import Foundation
+import KeychainSwift
 
-// TODO: Make Keychain confirm to 'SecureStorage'
+extension KeychainSwift: Storage {
+    
+    private static let encoder = JSONEncoder()
+    private static let decoder = JSONDecoder()
+    
+    public func store<T: Encodable>(_ value: T?, for key: AnyHashable) throws {
+        if let value {
+            let data = try Self.encoder.encode(value)
+            self.set(data, forKey: keyString(from: key))
+        } else {
+            removeValue(for: key)
+        }
+    }
+    
+    public func value<T: Decodable>(for key: AnyHashable) throws -> T {
+        guard let data = getData(keyString(from: key)) else {
+            throw NSError(domain: "Data missing for key: \(key)", code: 0)
+        }
+        return try Self.decoder.decode(T.self, from: data)
+    }
+    
+    public func removeValue(for key: AnyHashable) {
+        delete(keyString(from: key))
+    }
+    
+    private func keyString(from key: AnyHashable) -> String {
+        "\(key)"
+    }
+}
--- Template/Dependencies.swift
+++ Template/Dependencies.swift
@@ -2,6 +2,7 @@ import Foundation
 import Security
 import SwiftDependencyContainer
 import Core
+import KeychainSwift
 #if DEBUG
 import Onboarding
 import Todos
@@ -12,8 +13,7 @@ struct Dependencies: AutoSetup {
     
     func override(_ container: DependencyContainer) throws {
         try container.register(LocalStorage.self) { UserDefaults(suiteName: "Template_LocalStorage") }
-        // TODO: replace with Keychain once implemented
-        try container.register(SecureStorage.self) { UserDefaults(suiteName: "Template_SecureStorage") }
+        try container.register(SecureStorage.self) { KeychainSwift(keyPrefix: "Template_SecureStorage") }
         
         try container.register { ApiConfig(baseUrl: "https://example.server.com/api", authStore: resolve()) }
         
