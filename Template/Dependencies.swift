386562156d9dac080b4f0d2202ba640864b413d1
David Scheutz <scheutz.david+github@gmail.com>
2025-04-10 18:06:30 -0500

    improve dependencies documentation

--- Template/Dependencies.swift
+++ Template/Dependencies.swift
@@ -13,24 +13,25 @@ struct Dependencies: AutoSetup {
     
     func override(_ container: DependencyContainer) throws {
         /*
-         Examples of how to manually register third-party implementations
+         Examples of how to decouple third-party implementations
          */
-        // TODO: test new @Alias annotation
         try container.register(LocalStorage.self) { UserDefaults(suiteName: "Template_LocalStorage") }
         try container.register(SecureStorage.self) { KeychainSwift(keyPrefix: "Template_SecureStorage") }
         
         /*
-         Example of how to manually constructor inject another dependency
+         Example of how to manually register a dependency
          */
         try container.register { ApiConfig(baseUrl: "https://example.server.com/api", authStore: resolve()) }
         
         /*
-         Example of how to replace a component of your application,
-         in this case with a mock, which should be avoided but can be useful
-         e.g. when you have the API contract is ready but not yet hosted.
+         Example of how to replace a component of your application.
          */
         try container.register(HttpEngine.self) {
             #if DEBUG
+            /*
+             Mocks in general should be avoided, but can be useful in certain scenarios
+             e.g. when you have the API contract is defined but not yet available to the client.
+             */
             HttpEngineMock()
                 .register(OnboardingApiMock())
                 .register(TodoApiMock())
